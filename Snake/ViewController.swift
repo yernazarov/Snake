5248b1c658967bfacecb6a9996183e62018d1887
Zhandos <yernazarovz@gmail.com>
2020-12-07 21:20:34 +0500

    Initial Commit

---
+++ Snake/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snake
+//
+//  Created by Zhandos Yernazarov on 12/7/20.
+//  Copyright © 2020 Zhandos Yernazarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
